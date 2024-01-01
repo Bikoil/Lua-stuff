e59fe173254d55ff0d23fc5986db1a89b5944419
Biko <139659047+Bikoil@users.noreply.github.com>
2024-01-01 02:21:08 +0200

    Spaminator9000.lua
    Basically just a lua spam that uses while loops, they are all technically not randomised.

---
+++ Spaminator9000.lua
@@ -0,0 +1,45 @@
+--This was made in REPLIT, if you would like to check it out: https://replit.com/@bikoil
+local x = 1
+print("To spam random letters, type 'spam'")
+print("To spam numbers ascendingly, type 'numberspam'")
+print("To spam random emojis, type 'emojispam'")
+local input = io.read()
+if input == "spam" then
+  while true do
+    print("ahdjeb")
+    print("bemdoydydb")
+    print("ohdjbrgesi")
+    print("ehdjifbrjd")
+    print("abahsidj")
+    print("opwphbdb")
+    print("sjfkdueb")
+    print("pqiwhfbrh")
+    print("ehebrjd")
+    print("sosjebrhbr")
+    print("hajdbrhrbrbdi")
+    print("zbdueopwuh")
+  end
+end
+if input == "numberspam" then
+  while true do
+    print(x)
+    x = x + 1
+  end
+elseif input == "emojispam" then
+  while true do
+    print("😊🤯☺️🤨🍞")
+    print("🗣️🔪🔗🥰😍🤩")
+    print("🫥😝🤪😶‍🌫️🤐")
+    print("☹️🙁😳🥶🤯☹️")
+    print("🌛😷👾😎😷👿")
+    print("❣️💝💛😹😹🩵👺")
+    print("🤟👐🤧🤢🤢🤡")
+    print("🤷💁💆🧍🤼🏇🧑‍🦯")
+    print("👯👩‍❤️‍👨👨‍❤️‍👨👱🧑‍🎨😢😨")
+    print("🏞️🪨🪴🏖️🌦️☔👿😍😷")
+    print("🍡🫕🌮🌯🥙🥟")
+    print("🥖🧄🦅🦅🦃🐧")
+    print("🏟️🩱⛳🏑🥋🎮🎣")
+    print("🥁🪇🥁🪕🖌️🎻📻")
+  end
+end
